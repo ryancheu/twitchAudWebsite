f1300430fcc48ac285b7383424a8de0ba8e7809a
ryancheu <ryancheu@mit.edu>
2013-10-11 01:02:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitchAudWebsite::Application.config.secret_key_base = '1a24f6c9d710ac16a81c141a37a24c36f58f009cd8cb493621af175ff70c3d04c3247ccd1978ea22e6dba3ea6f29ee1b8b7df2cf82dd93e99a3545b75262b79e'
